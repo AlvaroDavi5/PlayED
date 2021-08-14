5ace26d90a1ec54cde88ba9246bd2c391b35918f
alvaro davi <alvarodavisa@gmail.com>
2021-08-14 18:04:42 -0300

    implemented playlist sourcecode

--- include/playlists.h
+++ include/playlists.h
@@ -2,12 +2,151 @@
 
 	#define _PLAYLISTS_H
 
-	/* type definitions */
-	typedef struct playlists_list Playlists;
+
+	/* ---------------------------- Type Definitions ---------------------------- */
+
+	// -------------- Abstract Data Types --------------
+	/*
+		size (int)
+		Playlist list pointers
+		- head
+		- tail
+	*/
+	typedef struct playlists_list PlaylistList;
+	/*
+		index (int)
+		name (string)
+		Playlist pointers
+		- prev
+		- next
+
+		size (int)
+		Music list pointers
+		- head
+		- tail
+	*/
 	typedef struct playlist Playlist;
+	/*
+		index (int)
+		artist (string)
+		name (string)
+		Music pointer
+		- next
+	*/
 	typedef struct music Music;
 
-	/* function prototypes */
-	int nonePL();
+	// -------------- Function Pointers --------------
+	// receive int,int and return int
+	typedef int (* fptrCompare)(int, int);
+
+
+
+	/* ---------------------------- Function Prototypes ---------------------------- */
+
+	// -------------- Music Manipulation --------------
+	/**
+		* @param string - name
+		* @param string - artist
+		* @return Music *
+	**/
+	Music * createMusic(char *name, char *artist);
+	/**
+		* @param Music * - song
+		* @return void
+	**/
+	void displayMusic(Music *song);
+
+	// -------------- Playlist Manipulation --------------
+	/**
+		* @param string - name
+		* @return Playlist *
+	**/
+	Playlist * initPlaylist(char *name);
+	/**
+		* @param Playlist* - playlist
+		* @param Music* - song
+		* @return void
+	**/
+	void addMusicToHead(Playlist *playlist, Music *song);
+	/**
+		* @param Playlist* - playlist
+		* @param Music* - song
+		* @return void
+	**/
+	void addMusicToTail(Playlist *playlist, Music *song);
+	/**
+		* @param Playlist* - playlist
+		* @param fptrCompare - compareFunction
+		* @param int - position
+		* @return Music *
+	**/
+	Music * getMusic(Playlist *playlist, fptrCompare compareFunction, int position);
+	/**
+		* @param Playlist* - playlist
+		* @param Music* - song
+		* @return void
+	**/
+	void deleteMusic(Playlist *playlist, Music *song);
+	/**
+		* @param Playlist* - playlist
+		* @return void
+	**/
+	void destroyPlaylist(Playlist *playlist);
+	/**
+		* @param Playlist* - playlist
+		* @return void
+	**/
+	void displayPlaylist(Playlist *playlist);
+
+	// -------------- PlaylistList Manipulation --------------
+	/**
+		* @param void
+		* @return PlaylistList *
+	**/
+	PlaylistList * initPlaylistList();
+	/**
+		* @param PlaylistList* - list
+		* @param Playlist* - pl
+		* @return void
+	**/
+	void addPlaylistToHead(PlaylistList *list, Playlist *pl);
+	/**
+		* @param PlaylistList* - list
+		* @param Playlist* - pl
+		* @return void
+	**/
+	void addPlaylistToTail(PlaylistList *list, Playlist *pl);
+	/**
+		* @param PlaylistList* - list
+		* @param Playlist* - pl
+		* @return Playlist *
+	**/
+	Playlist * getPlaylist(PlaylistList *list, fptrCompare compareFunction, int position);
+	/**
+		* @param PlaylistList* - list
+		* @param Playlist* - pl
+		* @return void
+	**/
+	void deletePlaylist(PlaylistList *list, Playlist *pl);
+	/**
+		* @param PlaylistList* - list
+		* @return void
+	**/
+	void destroyPlaylistList(PlaylistList *list);
+	/**
+		* @param PlaylistList* - list
+		* @return void
+	**/
+	void displayPlaylistList(PlaylistList *list);
+
+	// -------------- Another Functions --------------
+	/**
+		* Compare two values and return a status
+		* @param int - n1
+		* @param int - n2
+		* @return 0: n2 == n1, 1: n2 > n1, -1: n2 < n1
+	**/
+	int compareValue(int n1, int n2);
+
 
 #endif
--- source/playlists.c
+++ source/playlists.c
@@ -1,39 +1,360 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "playlists.h"
 
 
 struct playlists_list
 {
-	unsigned int size;
+	int size;
 	Playlist *head;
 	Playlist *tail;
 };
 
 struct playlist
 {
-	unsigned int index;
+	int index;
 	char *name;
 	Playlist *prev;
 	Playlist *next;
-	unsigned int size;
+	int size;
 	Music *head;
 	Music *tail;
 };
 
 struct music
 {
-	unsigned int index;
+	int index;
 	char *artist;
 	char *name;
 	Music *next;
 };
 
 
-int nonePL()
+/* -------------- Music Manipulation -------------- */
+Music * createMusic(char *name, char *artist)
 {
-	//
+	Music *song = (Music *) malloc(sizeof(Music));
 
-	return 0;
+	song->index = -1;
+	song->name = strdup(name);
+	song->artist = strdup(artist);
+	song->next = NULL;
+
+	return song;
 }
 
+void displayMusic(Music *song)
+{
+	printf("%s - %s \n", song->artist, song->name);
+}
+
+
+/* -------------- Playlist Manipulation -------------- */
+Playlist * initPlaylist(char *name)
+{
+	Playlist *playlist = (Playlist *) malloc(sizeof(Playlist));
+
+	playlist->index = -1;
+	playlist->name = strdup(name);
+	playlist->prev = NULL;
+	playlist->next = NULL;
+
+	playlist->size = 0;
+	playlist->head = NULL;
+	playlist->tail = NULL;
+
+	return playlist;
+}
+
+void addMusicToHead(Playlist *playlist, Music *song)
+{
+	if (playlist->head == NULL)
+	{
+		playlist->tail = song;
+		song->next = NULL;
+	}
+	else
+	{
+		song->next = playlist->head;
+	}
+
+	playlist->head = song;
+	playlist->size += 1;
+
+	Music *current = playlist->head;
+	for (int i = 0; i < playlist->size; i++)
+	{
+		(current)->index = i;
+		current = (current)->next;
+	}
+}
+
+void addMusicToTail(Playlist *playlist, Music *song)
+{
+	song->next = NULL;
+
+	if (playlist->head == NULL)
+	{
+		playlist->head = song;
+	}
+	else
+	{
+		playlist->tail->next = song;
+	}
+
+	song->index = playlist->size;
+	playlist->tail = song;
+	playlist->size += 1;
+}
+
+Music * getMusic(Playlist *playlist, fptrCompare compareFunction, int position)
+{
+	Music *current = playlist->head;
+
+	while (current != NULL)
+	{
+		if (compareFunction((current)->index, position) == 0)
+		{
+			return current;
+		}
+
+		current = (current)->next;
+	}
+
+	return NULL;
+}
+
+void deleteMusic(Playlist *playlist, Music *song)
+{
+	Music *current = playlist->head;
+
+	if (song == playlist->head)
+	{
+		if (playlist->head->next == NULL)
+		{
+			playlist->head = playlist->tail;
+		}
+		else
+		{
+			playlist->head = playlist->head->next;
+		}
+	}
+	else
+	{
+		while (current != NULL && (current)->next != song)
+		{
+			current = (current)->next;
+		}
+		if (current != NULL && (current)->next == song)
+		{
+			(current)->next = song->next;
+			((current)->next)->index = song->index;
+		}
+	}
+
+	free(song->artist);
+	free(song->name);
+	free(song);
+	playlist->size -= 1;
+
+	Music *indexedMusic = playlist->head;
+	for (int i = 0; i < playlist->size; i++)
+	{
+		if (indexedMusic != NULL)
+		{
+			indexedMusic->index = i;
+			indexedMusic = indexedMusic->next;
+		}
+	}
+}
+
+void destroyPlaylist(Playlist *playlist)
+{
+	Music *current = playlist->head;
+	Music *next = NULL;
+
+	while (current != NULL)
+	{
+		next = (current)->next;
+
+		deleteMusic(playlist, current);
+		current = next;
+	}
+
+	free(playlist->name);
+	free(playlist);
+}
+
+void displayPlaylist(Playlist *playlist)
+{
+	printf(" --- %s --- \n", playlist->name);
+
+	Music *current = playlist->head;
+
+	while (current != NULL)
+	{
+		displayMusic(current);
+		current = (current)->next;
+	}
+}
+
+
+/* -------------- PlaylistList Manipulation -------------- */
+PlaylistList * initPlaylistList()
+{
+	PlaylistList *list = (PlaylistList *) malloc(sizeof(PlaylistList));
+
+	list->size = 0;
+	list->head = NULL;
+	list->tail = NULL;
+
+	return list;
+}
+
+void addPlaylistToHead(PlaylistList *list, Playlist *pl)
+{
+	if (list->head == NULL)
+	{
+		list->tail = pl;
+		pl->next = NULL;
+	}
+	else
+	{
+		pl->next = list->head;
+	}
+
+	list->head = pl;
+	list->size += 1;
+
+	Playlist *current = list->head;
+	for (int i = 0; i < list->size; i++)
+	{
+		(current)->index = i;
+		current = (current)->next;
+	}
+}
+
+void addPlaylistToTail(PlaylistList *list, Playlist *pl)
+{
+	pl->next = NULL;
+
+	if (list->head == NULL)
+	{
+		list->head = pl;
+	}
+	else
+	{
+		list->tail->next = pl;
+	}
+
+	pl->index = list->size;
+	list->tail = pl;
+	list->size += 1;
+}
+
+Playlist * getPlaylist(PlaylistList *list, fptrCompare compareFunction, int position)
+{
+	Playlist *current = list->head;
+
+	while (current != NULL)
+	{
+		if (compareFunction((current)->index, position) == 0)
+		{
+			return current;
+		}
+
+		current = (current)->next;
+	}
+
+	return NULL;
+}
+
+void deletePlaylist(PlaylistList *list, Playlist *pl)
+{
+	Playlist *current = list->head;
+
+	if (pl == list->head)
+	{
+		if (list->head->next == NULL)
+		{
+			list->head = list->tail;
+		}
+		else
+		{
+			list->head = list->head->next;
+		}
+	}
+	else
+	{
+		while (current != NULL && (current)->next != pl)
+		{
+			current = (current)->next;
+		}
+		if (current != NULL && (current)->next == pl)
+		{
+			(current)->next = pl->next;
+			((current)->next)->index = pl->index;
+		}
+	}
+
+	free(pl->name);
+	free(pl);
+	list->size -= 1;
+
+	Playlist *indexedPlaylist = list->head;
+	for (int i = 0; i < list->size; i++)
+	{
+		if (indexedPlaylist != NULL)
+		{
+			indexedPlaylist->index = i;
+			indexedPlaylist = indexedPlaylist->next;
+		}
+	}
+}
+
+void destroyPlaylistList(PlaylistList *list)
+{
+	Playlist *current = list->head;
+	Playlist *next = NULL;
+
+	while (current != NULL)
+	{
+		next = (current)->next;
+
+		deletePlaylist(list, current);
+		current = next;
+	}
+
+	free(list);
+}
+
+void displayPlaylistList(PlaylistList *list)
+{
+	Playlist *current = list->head;
+
+	while (current != NULL)
+	{
+		printf("Playlist %d: \n", (current)->index);
+		displayPlaylist(current);
+		current = (current)->next;
+	}
+}
+
+
+/* -------------- Another Functions -------------- */
+int compareValue(int n1, int n2)
+{
+	if (n1 == n2)
+	{
+		return 0;
+	}
+	else if (n2 > n1)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
